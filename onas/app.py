530e7e5be57226218752899596161ab54dc22bee
Joel Morera <125975636+mxrera@users.noreply.github.com>
2024-05-15 19:58:33 +0200

    feat: add dynamic options

--- onas/app.py
+++ onas/app.py
@@ -16,6 +16,7 @@ class App(ctk.CTk):
         self.image = None
         self.codification = None
         self.codification_options = {}
+        self.options_list = []
         self.codification_steps = []
         self.results = {}
 
@@ -45,9 +46,23 @@ class App(ctk.CTk):
         ctk.CTkLabel(self.tabview.tab("settings"), text="Settings").grid(row=0, column=0, sticky=ctk.W + ctk.E)
         ctk.CTkLabel(self.tabview.tab("settings"), text="Image").grid(row=1, column=0, sticky=ctk.W + ctk.E + ctk.N + ctk.S)
         ctk.CTkButton(self.tabview.tab("settings"), text="Select", fg_color=BG_COLOR).grid(row=2, column=0, sticky=ctk.W + ctk.E)
-        ctk.CTkComboBox(self.tabview.tab("settings"), values=["JPEG"], command=self.on_codification_select, fg_color=BG_COLOR).grid(row=3, column=0, sticky=ctk.W + ctk.E)
+        
+        ctk.CTkComboBox(
+            self.tabview.tab("settings"),
+            values=["JPEG"],
+            command=self.on_codification_select,
+            fg_color=BG_COLOR
+        ).grid(row=3, column=0, sticky=ctk.W + ctk.E)
+        
         ctk.CTkLabel(self.tabview.tab("settings"), text="Codification Options").grid(row=4, column=0, sticky=ctk.W + ctk.E)
-        self.options_frame = ctk.CTkScrollableFrame(self.tabview.tab("settings"), fg_color=FG_COLOR).grid(row=5, column=0, sticky=ctk.N + ctk.E + ctk.S + ctk.W)
+        
+        self.options_frame = ctk.CTkScrollableFrame(
+            self.tabview.tab("settings"),
+            fg_color=FG_COLOR
+        )
+        self.options_frame.grid(row=5, column=0, sticky=ctk.N + ctk.E + ctk.S + ctk.W)
+        self.options_frame.grid_columnconfigure(0, weight=1)
+
         ctk.CTkButton(self.tabview.tab("settings"), text="Run", command=self.on_run, fg_color=BG_COLOR).grid(row=6, column=0, sticky=ctk.W + ctk.E)
 
     def __init_results_tab(self):
@@ -77,8 +92,7 @@ class App(ctk.CTk):
     def on_codification_select(self, codification):
         if codification == "JPEG":
             self.codification = JPEG()
-            self.codification_options = self.codification.options()
-            self.fill_codification_options()
+            self.fill_codification_options(self.codification.options())
 
     def on_run(self):
         if self.image is None or self.codification is None:
@@ -89,19 +103,36 @@ class App(ctk.CTk):
         self.results = self.codification(self.image)
         self.fill_results()
 
-    def fill_codification_options(self):
-        for widget in self.options_frame.winfo_children():
-            widget.destroy()
+    def fill_codification_options(self, options: dict):
+        # Clean previous options
+        for option in self.options_list:
+            option.destroy()
+        self.options_list = []
+        self.codification_options = {}
 
-        for key, value in self.codification_options.items():
+        # Create new options
+        for key, value in options.items():
+            # Set defaults of codification options
+            self.codification_options[key] = value["default"]
+            
+            # Fill options frame
+            option_frame = ctk.CTkFrame(self.options_frame, fg_color=BG_COLOR)
+            option_frame.grid(row=len(self.options_list), column=0, sticky=ctk.W + ctk.E)
+            option_frame.grid_columnconfigure(1, weight=1)
+            option_frame.grid_rowconfigure(0, weight=1, uniform="a")
+            option_frame.grid_rowconfigure(1, weight=1, uniform="a")
+            
+            ctk.CTkLabel(option_frame, text=key).grid(row=0, column=0, sticky=ctk.W + ctk.E)
             if value["type"] == "combobox":
-                ctk.CTkComboBox(self.options_frame, values=value["values"], fg_color=BG_COLOR).pack()
+                ctk.CTkComboBox(option_frame, values=value["values"], fg_color=BG_COLOR).grid(row=1, column=0, sticky=ctk.W + ctk.E)
             elif value["type"] == "spinbox":
-                ctk.CTkSpinBox(self.options_frame, values=value["values"], fg_color=BG_COLOR).pack()
+                slider = ctk.CTkSlider(option_frame, from_=value["values"][0], to=value["values"][1], number_of_steps=value["values"][2], fg_color=BG_COLOR)
+                slider.grid(row=1, column=0, sticky=ctk.W + ctk.E)
             elif value["type"] == "button":
-                ctk.CTkButton(self.options_frame, text="Select", fg_color=BG_COLOR)
-                
+                ctk.CTkButton(option_frame, text="Select", fg_color=BG_COLOR).grid(row=1, column=0, sticky=ctk.W + ctk.E)
 
+            self.options_list.append(option_frame)
+                
     def fill_results(self):
         for widget in self.results_frame.winfo_children():
             widget.destroy()
